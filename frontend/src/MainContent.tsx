d77ff1e4315acc9cfafbbdbfe65c91183e4bae10
kai <kai305021@gmail.com>
2025-08-07 01:37:22 +0800

    sidebar加入帳戶、建構登入畫面

--- frontend/src/MainContent.tsx
+++ frontend/src/MainContent.tsx
@@ -1,5 +1,6 @@
 import styles from "./MainContent.module.css";
 import { Routes, Route } from "react-router-dom";
+import Member from './pages/Member';
 import Home from './pages/Home';
 import AllTasks from "./pages/AllTasks";
 import CompletedTasks from "./pages/CompletedTasks";
@@ -10,6 +11,7 @@ function MainContent() {
   return (
     <main className={`${styles.mainContent} col-lg-10`}>
       <Routes>
+        <Route path="/Member" element={<Member />} />
         <Route path="/" element={<Home />} />
         <Route path="/AllTasks" element={<AllTasks />} />
         <Route path="/CompletedTasks" element={<CompletedTasks />} />
