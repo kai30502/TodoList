18fd1c5441920ccffa548e188ceb6c88c3351267
kai <kai305021@gmail.com>
2025-08-06 19:09:27 +0800

    修改tsconfig、建立express

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
